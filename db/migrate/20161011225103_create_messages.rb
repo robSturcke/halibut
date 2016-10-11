9635af666b3cc456b81990214b76d72171506f5f
Rob Sturcke <rs@rsvision.com>
2016-10-11 19:32:54 -0400

    user_id migration fix for schema

--- db/migrate/20161011225103_create_messages.rb
+++ db/migrate/20161011225103_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.references :chatroom, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :body
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008145754) do
+ActiveRecord::Schema.define(version: 20161011225103) do
 
   create_table "chatroom_users", force: :cascade do |t|
     t.integer  "chatroom_id"
@@ -29,10 +29,12 @@ ActiveRecord::Schema.define(version: 20161008145754) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "chatroom_id"
+    t.integer  "user_id"
     t.text     "body"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
